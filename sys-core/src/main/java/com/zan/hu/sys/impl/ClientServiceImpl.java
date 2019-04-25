d144e1279149841ce99bcfd7b4c95a982ec43ea5
jingxing <m17364550730@163.com>
2019-04-25 09:22:03 +0800

    增加commom包的引入

--- sys-core/src/main/java/com/zan/hu/sys/impl/ClientServiceImpl.java
+++ sys-core/src/main/java/com/zan/hu/sys/impl/ClientServiceImpl.java
@@ -1,18 +1,15 @@
 package com.zan.hu.sys.impl;
 
+import com.zan.hu.common.utils.BeanComUtils;
 import com.zan.hu.sys.ClientService;
 import com.zan.hu.sys.SysClient;
 import com.zan.hu.sys.domain.Client;
 import com.zan.hu.sys.mapper.ClientMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -39,7 +36,7 @@ public class ClientServiceImpl implements ClientService {
             throw new Exception("");
         }
         client = new Client();
-        BeanUtils.copyProperties(sysClient, client);
+        BeanComUtils.copyProperties(sysClient, client);
         client.setClientSecret(passwordEncoder.encode(client.getClientSecret()));
         clientMapper.insertSelective(client);
     }
@@ -48,8 +45,8 @@ public class ClientServiceImpl implements ClientService {
     public void updateClient(SysClient sysClient) throws IllegalAccessException {
         Client client = clientMapper.selectByClientId(sysClient.getClientId());
         if (client != null) {
-            String[] attributes = checkObjFieldIsNull(sysClient);
-            BeanUtils.copyProperties(sysClient, client, attributes);
+            String[] attributes = BeanComUtils.checkObjFieldIsNull(sysClient);
+            BeanComUtils.copyProperties(sysClient, client, attributes);
             clientMapper.updateByPrimaryKeySelective(client);
         }
     }
@@ -72,24 +69,4 @@ public class ClientServiceImpl implements ClientService {
     }
 
 
-    /**
-     *  is check attribute null
-     * @param obj
-     * @return
-     * @throws IllegalAccessException
-     */
-    public static String[] checkObjFieldIsNull(Object obj) throws IllegalAccessException {
-        if (obj == null)
-            return null;
-        List<String> attributes = new ArrayList<>();
-        for (Field f : obj.getClass().getDeclaredFields()) {
-            f.setAccessible(true);
-            log.info(f.getName());
-            if (f.get(obj) == null || f.get(obj).equals("")) {
-                attributes.add(f.getName());
-            }
-        }
-        String[] attribute = !StringUtils.isEmpty(attributes) ? attributes.toArray(new String[attributes.size()]) : null;
-        return attribute;
-    }
 }
